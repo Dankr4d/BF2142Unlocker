740ac8f44ce025515a46739e2c9c8179db79c7a3
Dankr4d <dude569@freenet.de>
2020-05-06 15:11:08 +0200

    - Added logic to use system language on startup when language wasn't   selected manually (regarding to #29)
    Signed-off-by: Dankr4d <dude569@freenet.de>

--- BF2142UnlockerLauncher.nim
+++ BF2142UnlockerLauncher.nim
@@ -5,6 +5,9 @@ when isMainModule and defined(windows):
   setCurrentDir(getCurrentDir() / "bin")
 
   ## Workarounds, because gettext is currently not working on windows: https://github.com/Dankr4d/BF2142Unlocker/issues/29
+  import strutils
+  proc setlocale(category: int, other: cstring): cstring {.header: "<locale.h>", importc.}
+  var LC_ALL {.header: "<locale.h>", importc.}: int
   const LANGUAGE_FILE: string = "lang.txt"
   const AVAILABLE_LANGUAGES: seq[string] = @["en_US.utf8", "de_DE.utf8"]
   const DEFAULT_LANGUAGE: string = "en_US.utf8"
@@ -14,7 +17,13 @@ when isMainModule and defined(windows):
   if lang != "" and lang in AVAILABLE_LANGUAGES:
     putEnv("LANG", lang)
   else:
-    putEnv("LANG", DEFAULT_LANGUAGE)
+    lang = $setlocale(LC_ALL, "")
+    if lang.startsWith("English"):
+      putEnv("LANG", "en_US.utf8")
+    elif lang.startsWith("German"):
+      putEnv("LANG", "de_DE.utf8")
+    else:
+      putEnv("LANG", DEFAULT_LANGUAGE)
   #
 
   discard execCmd("cmd /c " & "BF2142Unlocker.exe")
